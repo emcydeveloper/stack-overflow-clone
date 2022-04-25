8a2a0a0defb368779d2df776f57b4ff084dae94d
Ganesh Kanna MC <emcydeveloper@gmail.com>
2022-04-25 17:13:37 +0530

    votes

--- src/Components/DisplayQuestionAnswer.js
+++ src/Components/DisplayQuestionAnswer.js
@@ -2,29 +2,48 @@ import { Fragment } from "react";
 import { useState } from "react";
 import "./style.css";
 
-export default function DisplayQuestionAnswer({ questionId,getInfo, addAnswer }) {
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+
+
+export default function DisplayQuestionAnswer({
+  questionId,
+  getInfo,
+  addAnswer,
+}) {
   let userID = 1;
 
   let [textAreaValue, setTextAreaValue] = useState("");
   let [textAreaVisible, setTextAreaVisible] = useState(false);
-  let [like, setLike] = useState(0);
-  let [dislike, setDislike] = useState(like);
+  let [votes, setVotes] = useState(0);
+  
 
-  let handletexAreaVisisble = () => {setTextAreaVisible((prevState) => !prevState);setTextAreaValue("");};
-  let handleGetAnswer = (event) => {setTextAreaValue(event.target.value);};
+  let handletexAreaVisisble = () => {
+    setTextAreaVisible((prevState) => !prevState);
+    setTextAreaValue("");
+  };
+  let handleGetAnswer = (event) => {
+    setTextAreaValue(event.target.value);
+  };
   let handleSubmitAnswer = () => {
     addAnswer({
       type: "answer",
       createdBy: userID,
       questionID: questionId,
-      createdOn: `${new Date(Date.now()).getDate()}/${new Date(Date.now()).getMonth()+1}/${new Date(Date.now()).getFullYear()}`,
+      createdOn: `${new Date(Date.now()).getDate()}/${
+        new Date(Date.now()).getMonth() + 1
+      }/${new Date(Date.now()).getFullYear()}`,
       value: textAreaValue,
     });
     setTextAreaVisible((prevState) => !prevState);
     setTextAreaValue("");
   };
-  let handleLikeCount = ()=>{setLike((prevState)=>prevState+1)}
-  let handleDislikeCount = ()=>{setDislike((prevState)=>prevState-1)}
+  let handleLikeCount = () => {
+    setVotes((prevState) => prevState + 1);
+  };
+  let handleDislikeCount = () => {
+    setVotes((prevState) => prevState - 1);
+  };
 
   // useEffect(()=>console.log(textAreaValue),[textAreaValue])
   //{type:"question",createdBy:1,questionID:1,createdOn:"date",value:"What is my Name"}
@@ -33,46 +52,50 @@ export default function DisplayQuestionAnswer({ questionId,getInfo, addAnswer })
     <div className="display-ques-ans">
       {getInfo.map((getValue, i) => (
         <Fragment key={i}>
-
           {getValue.type === "question" && (
             <div className="display-ques-ans-ques">
-               <div className="ques-info">
-                <div><p>Created by:-</p><span>{getValue.createdBy}</span></div> 
-                <div><span>{like}</span><p><button onClick={handleLikeCount}>Like</button></p></div>
-                <div><span>{dislike}</span><p><button onClick={handleDislikeCount}>Dislike</button></p></div>
-                <div><p>Created on:-</p><span>{getValue.createdOn}</span></div>
-                
-              </div>
-
-             {/* <div className="ques-info">Created By:-{getValue.createdBy} Created on:-{getValue.createdOn}</div> */}
-              <div className="ques">{getValue.value}</div>
+              <div className="display-ques-ans-votes"><KeyboardArrowUpOutlinedIcon onClick={handleLikeCount} />{votes}<KeyboardArrowDownOutlinedIcon onClick={handleDislikeCount} /></div>
+              
+              <div className="ques">
+                <p>{getValue.value}</p>
+                <div className="ques-info">Submitted on {getValue.createdOn} by {getValue.createdBy}</div>
+                </div>
+              
               {/* {getValue.type}:- */}
             </div>
           )}
 
           {getValue.type === "answer" && (
             <div className="display-ques-ans-ans">
-              <div className="ans"><p>{getValue.value}</p></div>
-              <div className="ans-info">
-                <div><p>Answered by:-</p><span>{getValue.createdBy}</span></div> 
-                {/* <div><span>{count}</span><p><button onClick={handleLikeCount}>Like</button></p></div>
-                <div><span>{count}</span><p><button onClick={handleDislikeCount}>Dislike</button></p></div> */}
-                <div><p>Answered on:-</p><span>{getValue.createdOn}</span></div>
-                
+              <div className="ans">
+                <p>{getValue.value}</p>
               </div>
+              
+              <div className="ans-info">Submitted on {getValue.createdOn} by {getValue.createdBy}</div>
+              
             </div>
           )}
-
         </Fragment>
       ))}
       <div className="display-ques-ans-actions">
-        {textAreaVisible && (<textarea className="display-ques-ans-txtarea" value={textAreaValue} onChange={handleGetAnswer}></textarea>)}
-        {!textAreaVisible && (<button onClick={handletexAreaVisisble}>Answer</button>)}
+        {textAreaVisible && (
+          <textarea
+            className="display-ques-ans-txtarea"
+            value={textAreaValue}
+            onChange={handleGetAnswer}
+          ></textarea>
+        )}
+        {!textAreaVisible && (
+          <button onClick={handletexAreaVisisble}>Answer</button>
+        )}
         <div className="display-ques-ans-submit">
-          {textAreaVisible && (<button onClick={handleSubmitAnswer}>Submit</button>)}
-          {textAreaVisible && (<button onClick={handletexAreaVisisble}>Cancel</button>)}
+          {textAreaVisible && (
+            <button onClick={handleSubmitAnswer}>Submit</button>
+          )}
+          {textAreaVisible && (
+            <button onClick={handletexAreaVisisble}>Cancel</button>
+          )}
         </div>
-        
       </div>
     </div>
   );
